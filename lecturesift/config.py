7b235a0d1b4627d8c3d92cac87c5ff694cdc7708
ulasfirinciogullari-design <ulasfirinciogullari@gmail.com>
2026-08-27 04:22:08 +0300

    Add durable queue and object storage settings

--- lecturesift/config.py
+++ lecturesift/config.py
@@ -11,6 +11,14 @@ MAX_VIDEO_BYTES = int(os.getenv("LECTURESIFT_MAX_VIDEO_BYTES", str(1024 * 1024 *
 MAX_SOURCE_FILES = int(os.getenv("LECTURESIFT_MAX_SOURCE_FILES", "24"))
 JOB_TTL_SECONDS = int(os.getenv("LECTURESIFT_JOB_TTL_SECONDS", str(6 * 60 * 60)))
 
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "")
+REDIS_URL = os.getenv("REDIS_URL", CELERY_BROKER_URL)
+S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL", "")
+S3_REGION = os.getenv("S3_REGION", "auto")
+S3_BUCKET = os.getenv("S3_BUCKET", "")
+S3_ACCESS_KEY_ID = os.getenv("S3_ACCESS_KEY_ID", "")
+S3_SECRET_ACCESS_KEY = os.getenv("S3_SECRET_ACCESS_KEY", "")
+
 VIDEO_EXTENSIONS = {".mp4", ".mov", ".mkv", ".webm", ".mpeg", ".mpg", ".m4v"}
 
 LANGUAGE_NAMES = {
