7c882d0366076b43e7bf8b9c0c9313368b448a2f
ulasfirinciogullari-design <ulasfirinciogullari@gmail.com>
2026-08-28 00:08:00 +0300

    feat: install Instagram launch routes

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 from lecturesift.app import app
 from lecturesift.email_auth import install_email_auth
+from lecturesift.social_routes import install_social_routes
 
 install_email_auth(app)
+install_social_routes(app)
 
 __all__ = ["app"]
